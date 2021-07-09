ee6979d2536ebc0653e81d2f5913245a6e7bd8ac
yifeiwangG100 <73860709+andyfadai@users.noreply.github.com>
2021-07-09 20:01:34 +1000

    initial framework of the api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+const exphbs = require("express-handlebars")
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => {
+    console.log('The small games app is running')
+})
