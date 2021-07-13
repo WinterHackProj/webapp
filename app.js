3512c1d230be84476566aa1582fe616414c24059
yifeiwangG100 <yifeiandywang@outlook.com>
2021-07-13 22:46:00 +1000

    index and login page created

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login', { layout: "login-layout" })
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('The small games app is running')
 })
