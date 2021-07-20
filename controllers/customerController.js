c95128197af9a2374df962fbbdac13d442ae1276
yifeiwangG100 <73860709+andyfadai@users.noreply.github.com>
2021-07-20 12:22:29 +1000

    portfolio UI changed and try-catch err added

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -5,57 +5,87 @@ const customer = require("../models/customer")
 const Customer = customer.Customer
 
 const getIndex = async(req, res) => {
-    if (req.session.email) {
-        const customer = await Customer.findOne({ "email": req.session.email }).lean()
-        const subjects = customer.subjects
-        const subjectOnIndex = []
+    try {
+        if (req.session.email) {
+            const customer = await Customer.findOne({ "email": req.session.email }).lean()
+            const subjects = customer.subjects
+            const subjectOnIndex = []
 
-        for (var i = 0; i < subjects.length; i++) {
-            var oneSubject = await Subject.findOne({ "_id": subjects[i].SubjectId }).lean()
-            subjectOnIndex.push(oneSubject)
+            for (var i = 0; i < subjects.length; i++) {
+                var oneSubject = await Subject.findOne({ "_id": subjects[i].SubjectId }).lean()
+                subjectOnIndex.push(oneSubject)
+            }
+            if (customer === null) {
+                res.status(404)
+                return res.send('Food not found')
+            }
+            res.render('index', { "thiscustomer": customer, "subjectOnIndex": subjectOnIndex })
+        } else {
+            res.render('index')
         }
-        if (customer === null) {
-            res.status(404)
-            return res.send('Food not found')
-        }
-        res.render('index', { "thiscustomer": customer, "subjectOnIndex": subjectOnIndex })
-    } else {
-        res.render('index')
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
     }
 }
 
 // GET the profile of a customer
 const getPortfolio = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    res.render('portfolio', { "thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        res.render('portfolio', { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 // GET change informaiton page
 const changeInfoGet = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    res.render('change-info', { "thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        res.render('change-info', { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 // POST change informaiton page
 const changeInfoPost = async(req, res) => {
-    await Customer.updateOne({ "email": req.session.email }, { "nickName": req.body.nickName}).lean()
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    res.render('changeOutcome', { "message": "Nick name changed.", "thiscustomer": customer })
+    try {
+        await Customer.updateOne({ "email": req.session.email }, { "nickName": req.body.nickName }).lean()
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        res.render('changeOutcome', { "message": "Nick name changed.", "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 // GET change password 
 const changePasswordGet = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    res.render('change-password', { "thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        res.render('change-password', { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 // POST change password 
 const changePasswordPost = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    var oneCustomer = new Customer();
-    var passw = oneCustomer.generateHash(req.body.password2)
-    await Customer.findOneAndUpdate({ "email": req.session.email }, { "password": passw }).lean()
-    res.render('changeOutcome', { "message": "Password changed.","thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        var oneCustomer = new Customer();
+        var passw = oneCustomer.generateHash(req.body.password2)
+        await Customer.findOneAndUpdate({ "email": req.session.email }, { "password": passw }).lean()
+        res.render('changeOutcome', { "message": "Password changed.", "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 module.exports = {
     getIndex,
--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -20,166 +20,195 @@ const getAddSubject = async(req, res) => {
     try {
         const customer = await Customer.findOne({ "email": req.session.email }).lean()
         res.render('add-subject', { "thiscustomer": customer })
-    } catch {
+    } catch (err) {
         console.log(err)
-        return res.send("cannot GET the subject")
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
     }
 }
 
 const addSubject = async(req, res) => {
-    // try{
-    // current customer
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-        // the subjects the current customer has
-    const subjects = customer.subjects
-        //save the subjects
-    var postData = {
-        subject_code: req.body.subject_code.toUpperCase(),
-        subject_name: req.body.subject_name,
-        credit: req.body.credit,
-        target: req.body.target,
-        assignments: []
-    }
-    var subject = new Subject(postData)
-    subjectRecord = new CustomerSubject({ SubjectId: subject._id, subject_code: req.body.subject_code.toUpperCase() })
-        // new user
-    if (subjects.length === 0) {
-        subjects.push(subjectRecord)
-        await Customer.updateOne({ "email": req.session.email }, { "subjects": subjects }).lean()
-        await subject.save()
-    } else {
-        // check if the subject is already in canvas
-        for (var i = 0; i < subjects.length; i++) {
-            if (req.body.subject_code.toUpperCase() === subjects[i].subject_code) {
-                return res.render("cannotAddSubjectError")
-            }
+    try {
+        // current customer
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+            // the subjects the current customer has
+        const subjects = customer.subjects
+            //save the subjects
+        var postData = {
+            subject_code: req.body.subject_code.toUpperCase(),
+            subject_name: req.body.subject_name,
+            credit: req.body.credit,
+            target: req.body.target,
+            assignments: []
         }
-        subjects.push(subjectRecord)
-        await Customer.updateOne({ "email": req.session.email }, { "subjects": subjects }).lean()
-        await subject.save()
+        var subject = new Subject(postData)
+        subjectRecord = new CustomerSubject({ SubjectId: subject._id, subject_code: req.body.subject_code.toUpperCase() })
+            // new user
+        if (subjects.length === 0) {
+            subjects.push(subjectRecord)
+            await Customer.updateOne({ "email": req.session.email }, { "subjects": subjects }).lean()
+            await subject.save()
+        } else {
+            // check if the subject is already in canvas
+            for (var i = 0; i < subjects.length; i++) {
+                if (req.body.subject_code.toUpperCase() === subjects[i].subject_code) {
+                    return res.render("cannotAddSubjectError")
+                }
+            }
+            subjects.push(subjectRecord)
+            await Customer.updateOne({ "email": req.session.email }, { "subjects": subjects }).lean()
+            await subject.save()
+        }
+        return res.render('addSubjectSuccess', { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
     }
-    return res.render('addSubjectSuccess', { "thiscustomer": customer })
-        // } 
-        // catch{
-        //     console.log(err)
-        //     return res.send("cannot add the subject")
-        // }
 
 }
 
 const deleteSubject = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    await Customer.updateOne({ "email": req.session.email }, { $pull: { subjects: { "SubjectId": req.session.subjectId } } }).lean()
-    await Subject.deleteOne({ "_id": req.session.subjectId }).lean()
-    return res.render('deleteSubject', { "thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        await Customer.updateOne({ "email": req.session.email }, { $pull: { subjects: { "SubjectId": req.session.subjectId } } }).lean()
+        await Subject.deleteOne({ "_id": req.session.subjectId }).lean()
+        return res.render('deleteSubject', { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 //save subject target grades
 const saveTargerGrade = (req, res) => {
-    var subject_code = Object.keys(req.body)[0];
-    var target_grade = Object.values(req.body)[0];
-    Subject.updateOne({ subject_code: { $eq: subject_code } }, { target: target_grade },
-        function(err, docs) {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log("Updated Docs:", docs);
+    try {
+        var subject_code = Object.keys(req.body)[0];
+        var target_grade = Object.values(req.body)[0];
+        Subject.updateOne({ subject_code: { $eq: subject_code } }, { target: target_grade },
+            function(err, docs) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Updated Docs:", docs);
+                }
             }
-        }
-    )
-    res.redirect('back')
+        )
+        res.redirect('back')
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 const doCalculation = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    const subjectInfo = await Subject.findOne({ "_id": req.body.subjectId }).lean()
-    console.log(req.body)
-    var indicesEmpty = []
-    var assignmentNames = req.body.assignmentName
-    var obtainedScore = req.body.obtainedScore
-    var targetScore = new Array(obtainedScore.length)
-    var totalScore = req.body.totalScore
-    var overallTarget = req.body.overallTarget
-    var sumScore = 0
-    var totalRemaining = 0
-    console.log("计算前数据: " + obtainedScore)
-    for (var i = 0; i < obtainedScore.length; i++) {
-        if (!obtainedScore[i]) {
-            indicesEmpty.push(i)
-            totalRemaining += parseFloat(totalScore[i])
-        } else {
-            sumScore += parseFloat(obtainedScore[i])
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        const subjectInfo = await Subject.findOne({ "_id": req.body.subjectId }).lean()
+        console.log(req.body)
+        var indicesEmpty = []
+        var assignmentNames = req.body.assignmentName
+        var obtainedScore = req.body.obtainedScore
+        var targetScore = new Array(obtainedScore.length)
+        var totalScore = req.body.totalScore
+        var overallTarget = req.body.overallTarget
+        var sumScore = 0
+        var totalRemaining = 0
+        console.log("计算前数据: " + obtainedScore)
+        for (var i = 0; i < obtainedScore.length; i++) {
+            if (!obtainedScore[i]) {
+                indicesEmpty.push(i)
+                totalRemaining += parseFloat(totalScore[i])
+            } else {
+                sumScore += parseFloat(obtainedScore[i])
+            }
         }
+        console.log("indices_empty: " + indicesEmpty)
+        console.log("sumScore: " + sumScore)
+        console.log("totalRemaining: " + totalRemaining)
+
+        var requiredEach = overallTarget - sumScore
+        console.log("requiredEach: " + requiredEach)
+        for (var i = 0; i < indicesEmpty.length; i++) {
+            targetScore[indicesEmpty[i]] = allocateRemainingScore(requiredEach, totalScore[indicesEmpty[i]], totalRemaining)
+        }
+        console.log("计算后数据: " + obtainedScore)
+
+        console.log(targetScore)
+        var assignmentInfo = []
+        var allAssignments = []
+
+        for (var i = 0; i < assignmentNames.length; i++) {
+            var assignmentEntity = {}
+            assignmentEntity.name = assignmentNames[i]
+            assignmentEntity.percentage = totalScore[i]
+            assignmentEntity.current_score = obtainedScore[i]
+            assignmentEntity.target = targetScore[i]
+            assignmentInfo.push(assignmentEntity)
+            var assignment = new Assignment(assignmentEntity)
+            var assignmentRecord = new subjectAssignment({ assignmentId: assignment._id })
+            allAssignments.push(assignmentRecord)
+            await Subject.updateOne({ "_id": req.body.subjectId }, { "assignments": allAssignments }).lean()
+            await assignment.save()
+        }
+        console.log(assignmentInfo)
+
+        res.render('subject-detail', { "subjectInfo": subjectInfo, "thiscustomer": customer, "assignmentInfo": assignmentInfo })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
     }
-    console.log("indices_empty: " + indicesEmpty)
-    console.log("sumScore: " + sumScore)
-    console.log("totalRemaining: " + totalRemaining)
-
-    var requiredEach = overallTarget - sumScore
-    console.log("requiredEach: " + requiredEach)
-    for (var i = 0; i < indicesEmpty.length; i++) {
-        targetScore[indicesEmpty[i]] = allocateRemainingScore(requiredEach, totalScore[indicesEmpty[i]], totalRemaining)
-    }
-    console.log("计算后数据: " + obtainedScore)
-
-    console.log(targetScore)
-    var assignmentInfo = []
-    var allAssignments = []
-
-    for (var i = 0; i < assignmentNames.length; i++) {
-        var assignmentEntity = {}
-        assignmentEntity.name = assignmentNames[i]
-        assignmentEntity.percentage = totalScore[i]
-        assignmentEntity.current_score = obtainedScore[i]
-        assignmentEntity.target = targetScore[i]
-        assignmentInfo.push(assignmentEntity)
-        var assignment = new Assignment(assignmentEntity)
-        var assignmentRecord = new subjectAssignment({ assignmentId: assignment._id })
-        allAssignments.push(assignmentRecord)
-        await Subject.updateOne({ "_id": req.body.subjectId }, { "assignments": allAssignments }).lean()
-        await assignment.save()
-    }
-    console.log(assignmentInfo)
-
-    res.render('subject-detail', { "subjectInfo": subjectInfo, "thiscustomer": customer, "assignmentInfo": assignmentInfo })
 }
 
 const getEachSubject = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    const subjectInfo = await Subject.findOne({ "_id": req.params._id }).lean()
-    const allAssignments = subjectInfo.assignments
-    if (allAssignments.length == 0) { // Currently no assignments for this subject
-        var assignmentInfo = [{ "assName": "", "myScore": "", "percentage": "", "target": "" }]
-    } else {
-        var assignmentInfo = []
-        for (var i in allAssignments) {
-            var assignmentId = allAssignments[i].assignmentId
-            assignmentInfo.push(await Assignment.findOne({ "_id": assignmentId }).lean())
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        const subjectInfo = await Subject.findOne({ "_id": req.params._id }).lean()
+        const allAssignments = subjectInfo.assignments
+        if (allAssignments.length == 0) { // Currently no assignments for this subject
+            var assignmentInfo = [{ "assName": "", "myScore": "", "percentage": "", "target": "" }]
+        } else {
+            var assignmentInfo = []
+            for (var i in allAssignments) {
+                var assignmentId = allAssignments[i].assignmentId
+                assignmentInfo.push(await Assignment.findOne({ "_id": assignmentId }).lean())
+            }
+            console.log(assignmentInfo)
         }
-        console.log(assignmentInfo)
+        req.session.subjectId = req.params._id
+        res.render('subject-detail', { "subjectInfo": subjectInfo, "thiscustomer": customer, "assignmentInfo": assignmentInfo })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
     }
-    req.session.subjectId = req.params._id
-    res.render('subject-detail', { "subjectInfo": subjectInfo, "thiscustomer": customer, "assignmentInfo": assignmentInfo })
 }
 
 const addScore = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    const subjectInfo = await Subject.findOne({ "_id": req.body.subject }).lean()
-    var assignList = req.body.assignments.split(",")
-    var percentList = req.body.percentages.split(",")
-    var scoresList = req.body.scores.split(",")
-    var targetsList = req.body.targets.split(",")
-    var assLength = assignList.length
-    var subjectId = req.body.subject
-    var gradeInfo = { assignList, percentList, scoresList, targetsList, assLength, subjectId }
-    console.log(gradeInfo)
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        const subjectInfo = await Subject.findOne({ "_id": req.body.subject }).lean()
+        var assignList = req.body.assignments.split(",")
+        var percentList = req.body.percentages.split(",")
+        var scoresList = req.body.scores.split(",")
+        var targetsList = req.body.targets.split(",")
+        var assLength = assignList.length
+        var subjectId = req.body.subject
+        var gradeInfo = { assignList, percentList, scoresList, targetsList, assLength, subjectId }
+        console.log(gradeInfo)
 
-    res.render('subjectPage', { "subjectInfo": subjectInfo, "thiscustomer": customer, "gradeInfo": gradeInfo })
+        res.render('subjectPage', { "subjectInfo": subjectInfo, "thiscustomer": customer, "gradeInfo": gradeInfo })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 const getCalcInfo = async(req, res) => {
-    const customer = await Customer.findOne({ "email": req.session.email }).lean()
-    res.render("intro", { "thiscustomer": customer })
+    try {
+        const customer = await Customer.findOne({ "email": req.session.email }).lean()
+        res.render("intro", { "thiscustomer": customer })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).render('error', { errorCode: '400', layout: 'error-layout' })
+    }
 }
 
 // Export the functions
